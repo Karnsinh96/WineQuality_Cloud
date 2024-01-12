c3f3a030ea810e57f49936a0b7b1723440401dd1
Karnsinh Chikane <karnsinh96@gmail.com>
2024-01-12 17:33:01 +0530

    Data Ingestio and Validation Stages Completed...

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from mlProject import logger
-from mlProject.pipeline.stage_01_data_ingestion import *
+from mlProject.pipeline.stage01_data_ingestion_pipeline import *
 
 #Stage 1 Data Ingestion
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+from mlProject.pipeline.stage02_data_validation_pipeline import *
+#State 2 Data Validation
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -4,6 +4,8 @@ from mlProject.utils.common import read_yaml, create_directories
 
 #DataIngestionConfig need to import from config_entity
 from mlProject.entity.config_entity import DataIngestionConfig
+#DataVAlidateConfig need to import from config_entity
+from mlProject.entity.config_entity import DataValidationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -18,7 +20,7 @@ class ConfigurationManager:
 
         create_directories([self.config.artifacts_root])
 
-
+    #Data Ingestion
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
 
@@ -30,5 +32,19 @@ class ConfigurationManager:
             local_data_file=config.local_data_file,
             unzip_dir=config.unzip_dir 
         )
-
         return data_ingestion_config
+    
+    #Data Validation
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+        return data_validation_config
