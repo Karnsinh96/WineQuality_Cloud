a3612d79fa1a34ebdfc45940f34ce8b82d4c7ebf
Karnsinh Chikane <karnsinh96@gmail.com>
2024-01-13 15:38:43 +0530

    Data Transformation Completed Successfully done...

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -6,6 +6,8 @@ from mlProject.utils.common import read_yaml, create_directories
 from mlProject.entity.config_entity import DataIngestionConfig
 #DataVAlidateConfig need to import from config_entity
 from mlProject.entity.config_entity import DataValidationConfig
+#DataTransformation need to import from config_entity
+from mlProject.entity.config_entity import DataTransformationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -48,3 +50,16 @@ class ConfigurationManager:
             all_schema=schema,
         )
         return data_validation_config
+    
+    #Data Transformation
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+        return data_transformation_config
+    
